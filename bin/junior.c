d07e82a16a20b48b778d4b9c7f4b7a36826d3a39
naltun <nbaltunian@gmail.com>
2015-10-02 15:02:08 +0100

    Add lval_expr_print and lval_print

--- bin/junior.c
+++ bin/junior.c
@@ -148,8 +148,37 @@ void lval_print(lval v) {
   free(v);
 }
 
-/* Prints a "lisp_value" with a newline */
-void Lval_printlin(lval v) { lval_print(v); putchar('\n'); }
+/* Forward declaration of lval_print function on line */
+/* This is done in order to call lval_print in the lval_expr_print function before the arguments get defined */
+void lval_print(lval* v);
+
+void lval_expr_print(lval* v, char open, char close) {
+  putchar(open);
+  for (int i = 0; i < v->count; i++) {
+
+    /* Prints value */
+    lval_print(v->cell[i]);
+
+    /* If the last element has trailing space, doesn't print */
+    if (i != (v->count - 1)) {
+      putchar(' ');
+    }
+  }
+  putchar(close);
+}
+
+void lval_print(lval* v) {
+  switch(v->type) {
+
+    case LVAL_NUM: printf("%li", v->num); break;
+    case LVAL_ERR: printf("Error! %s", v->err); break;
+    case LVAL_SYM: printf("%s", v->sym); break;
+    case LVAL_SEXPR: lval_expr_print(v, '(', ')'); break;
+  }
+}
+
+/* Prints lval with a newline */
+void lval_println(lval* v) { lval_print(v); putchar('\n'); }
 
 lval eval_op(lval x, char* op, lval y) {
 
