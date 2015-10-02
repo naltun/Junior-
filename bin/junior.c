a7dc7ef5ebe38c2a024852a10e9d04c807eb569f
naltun <nbaltunian@gmail.com>
2015-10-02 13:46:24 +0100

    Add read expressions
    lval_read_num and lval_read recursivly looks at each node of the abstract syntax tree, and constructs an S-Expression.

--- bin/junior.c
+++ bin/junior.c
@@ -209,6 +209,38 @@ lval eval(mpc_ast_t* t) {
   return x;
 }
 
+lval* lval_read_num(mpc_ast_t* t) {
+  errno = 0;
+  long x = strtol(t->contents, NULL, 10);
+  return errno != ERANGE ?
+    lval_num(x) : lval_err("invailid number!");
+}
+
+lval* lval_read(mpc_ast_t* t) {
+
+  /* If Symbol or a Number, returns a conversion to that type */
+  if (strstr(t->tag, "number")) { return lval_read_num(t); }
+  if (strstr(t->tag, "symbol")) { return lval_sym(t->contents); }
+
+  /* If the root is '>' or sexpr then creates empty list */
+  lval* x = NULL;
+  if (strcmp(t->tag, ">") == 0) { x = lval_sexpr(); }
+  if (strcmp(t->tag, "sexpr"))  { x = lval_sexpr(); }
+
+  /* Fill the list with any valid expression passed through */
+  for (int i = 0; i < t->children_num; i++) {
+    if (strcmp(t->children[i]->contents, "(") == 0) { continue; }
+    if (strcmp(t->children[i]->contents, ")") == 0) { continue; }
+    if (strcmp(t->children[i]->contents, "}") == 0) { continue; }
+    if (strcmp(t->children[i]->contents, "{") == 0) { continue; }
+    if (strcmp(t->children[i]->tag,  "regex") == 0) { continue; }
+
+    x = lval_add(x, lval_read(t->children[i]));
+
+    return x;
+  }
+}
+
 int main(int argc, char** argv) {
 
   /* Create Parsers */
