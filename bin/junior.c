4de0af68da95cc16ae21964e01d987a4e0bde3e3
naltun <nbaltunian@gmail.com>
2015-10-03 12:04:41 +0100

    Add builtin_op function
    builtin_op takes a single lval* representing a list of all arguments to operate on, and checks for errors. builtin_op is required to allow for more thorough error checking.

--- bin/junior.c
+++ bin/junior.c
@@ -243,6 +243,56 @@ void lval_print(lval* v) {
 /* Prints lval with a newline */
 void lval_println(lval* v) { lval_print(v); putchar('\n'); }
 
+lval* builtin_op(lval* a, char* op) {
+
+  // Checks that all arguments are numbers
+  for (int i = 0; i < a->count; i++) {
+    if (a->cell[i]->type != LVAL_NUM) {
+      lval_del(a);
+      return lval_err("Cannot operate on a non-number!");
+    }
+  }
+
+  // Pops the first element
+  lval* x = lval_pop(a, 0);
+
+  // If there are no arguments and sub then perform a unary negation
+  if ((strcmp(op, "-") == 0) && a->count == 0) {
+    x->num = -x->num;
+  }
+
+  // While further elements remain
+  while (a->count > 0) {
+
+    // Pops the next element as before
+    lval* y = lval_pop(a, 0);
+
+    if (strcmp(op, "+") == 0) { x->num += y->num; }
+    if (strcmp(op, "-") == 0) { x->num -= y->num; }
+    if (strcmp(op, "*") == 0) { x->num *= y->num; }
+    if (strcmp(op, "/") == 0) {
+      if (y->num == 0) {
+        lval_del(x); lval_del(y);
+        x = lval_err("Error: you can't divide by Zero!"); break;
+      }
+      x->num /= y->num;
+    }
+
+    if (strcmp(op, "%") == 0) {
+      if (y->num == 0) {
+        lval_del(x); lval_del(y);
+        x = lval_err("Error: cannot perform modulus with Zero!"); break;
+      }
+      x->num = x->num % y->num;
+    }
+
+    lval_del(y);
+  }
+
+  lval_del(a);
+  return x;
+}
+
 lval eval_op(lval x, char* op, lval y) {
 
   /* If either the x or y value is an error, return value */
